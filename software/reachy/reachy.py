be84fefe150349fb796d7854e0e8f5aeced323b8
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-02-07 18:54:46 +0100

    Setup reachy with low moving speed by default.

--- software/reachy/reachy.py
+++ software/reachy/reachy.py
@@ -9,3 +9,10 @@ class Reachy(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(sum, axis=0)
+
+        robot.joint_motors = [m for m in robot.motors if m.name != 'finger_grip']
+        for m in robot.joint_motors:
+            m.moving_speed = 50
+
+        robot.finger_grip.goal_position = 0
+        robot.finger_grip.moving_speed = 0
