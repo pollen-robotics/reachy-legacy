88cf4cd9791219d301b9cfd4bcaf89e85ca300ba
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-19 09:46:25 +0200

    Typo in URDF path.

--- software/test_ik.py
+++ software/test_ik.py
@@ -1,6 +1,6 @@
 import ikpy
 
 chain = ikpy.chain.Chain.from_urdf_file(
-    urdf_file='./hardware/URDF/robots/reachy.URDF',
+    urdf_file='../hardware/URDF/robots/reachy.URDF',
     base_elements=['base'],
 )
