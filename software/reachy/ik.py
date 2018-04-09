d2586fe82ef0f7f885abc9ecc3834ab693910cb8
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-04-09 14:30:55 +0200

    Fix ik conversion.

--- software/reachy/ik.py
+++ software/reachy/ik.py
@@ -14,10 +14,11 @@ class IkChain(object):
         return self._chain.end_effector
 
     def forward_kinematics(self, joints):
-        return self._chain.forward_kinematics([0] + list(joints) + [0])
+        return self._chain.forward_kinematics(self._chain.convert_to_ik_angles(joints))
 
     def inverse_kinematics(self, target, initial_position=None, accurate=False):
         opt = {}
         if not accurate:
             opt['max_iter'] = 3
+        target = self._chain.convert_to_ik_angles(target)
         return self._chain.inverse_kinematics(target, initial_position, **opt)[1:-1]
