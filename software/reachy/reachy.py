bf2ea048fd0132962e34671f16d7f68c8afd4488
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-04-10 19:05:55 +0200

    another fix.

--- software/reachy/reachy.py
+++ software/reachy/reachy.py
@@ -16,41 +16,44 @@ from .primitives import (Record, Play,
 from .ik import IkChain
 
 
+def setup(robot):
+    robot._primitive_manager._filter = partial(sum, axis=0)
+
+    for m in robot.motors:
+        m.moving_speed = 50
+
+    robot.attach_primitive(TiringDemo(robot), 'tiring_demo')
+    robot.attach_primitive(TurnCompliant(robot), 'turn_compliant')
+    robot.attach_primitive(GotoRest(robot), 'goto_rest')
+    robot.attach_primitive(Idle(robot), 'idle')
+
+    robot.attach_primitive(Record(robot), 'record')
+    robot.attach_primitive(Play(robot), 'play')
+
+    if robot.simulated:
+        vrep_io = robot._controllers[0].io
+        robot.is_colliding = lambda: vrep_io.get_collision_state('Collision')
+
+        robot.last_collision = None
+        robot.recent_collisions = deque([], 10)
+
+        def did_collide():
+            while True:
+                if robot.is_colliding:
+                    t = time.time()
+                    robot.last_collision = t
+                    robot.recent_collisions.append(t)
+                time.sleep(0.02)
+
+        t = Thread(target=did_collide)
+        t.daemon = True
+        t.start()
+
+
 class Reachy(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
-        robot._primitive_manager._filter = partial(sum, axis=0)
-
-        for m in robot.motors:
-            m.moving_speed = 50
-
-        robot.attach_primitive(TiringDemo(robot), 'tiring_demo')
-        robot.attach_primitive(TurnCompliant(robot), 'turn_compliant')
-        robot.attach_primitive(GotoRest(robot), 'goto_rest')
-        robot.attach_primitive(Idle(robot), 'idle')
-
-        robot.attach_primitive(Record(robot), 'record')
-        robot.attach_primitive(Play(robot), 'play')
-
-        if robot.simulated:
-            vrep_io = robot._controllers[0].io
-            robot.is_colliding = lambda: vrep_io.get_collision_state('Collision')
-
-            robot.last_collision = None
-            robot.recent_collisions = deque([], 10)
-
-            def did_collide():
-                while True:
-                    if robot.is_colliding:
-                        t = time.time()
-                        robot.last_collision = t
-                        robot.recent_collisions.append(t)
-                    time.sleep(0.02)
-
-            t = Thread(target=did_collide)
-            t.daemon = True
-            t.start()
-
+        setup(robot)
         robot.ik_chain = IkChain(robot, tip=[0, 0, -0.02409])
 
 
@@ -76,6 +79,7 @@ def Leachy(*args, **kwargs):
             robot.config = json.load(f)
         urdf_file = os.path.join(os.path.dirname(__file__), 'leachy.urdf')
         robot.urdf_file = urdf_file
+        setup(robot)
 
     robot.urdf_file = robot.urdf_file.replace('reachy.urdf', 'leachy.urdf')
     robot.ik_chain = IkChain(robot, tip=[0, 0, -0.8])
