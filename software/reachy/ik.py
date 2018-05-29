98bfd3eece8ce7210f416e8689ae077fa78f5130
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-05-29 15:52:30 +0200

    Fix kinematics angles conversion.

--- software/reachy/ik.py
+++ software/reachy/ik.py
@@ -1,3 +1,5 @@
+from numpy import deg2rad
+
 from pypot.creatures import ik
 
 
@@ -13,12 +15,16 @@ class IkChain(object):
     def end_effector(self):
         return self._chain.end_effector
 
+    def convert_to_ik_angles(self, joints):
+        """ Convert to IKPY internal representation. """
+        return [0] + [deg2rad(j) for j in joints] + [0]
+
     def forward_kinematics(self, joints):
-        return self._chain.forward_kinematics(self._chain.convert_to_ik_angles(joints))
+        return self._chain.forward_kinematics(self.convert_to_ik_angles(joints))
 
     def inverse_kinematics(self, target, initial_position=None, accurate=False):
         opt = {}
         if not accurate:
             opt['max_iter'] = 3
-        target = self._chain.convert_to_ik_angles(target)
+        target = self.convert_to_ik_angles(target)
         return self._chain.inverse_kinematics(target, initial_position, **opt)[1:-1]
